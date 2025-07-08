6c0a779de68c6e15b8b0effe564dec4d52c3551e
Yuichi Umemoto <y.umemoto@000088-M.local>
2025-07-08 15:03:56 +0900

    parametrize pipeline

--- steps/04_orchestrate_jobs.sql
+++ steps/04_orchestrate_jobs.sql
@@ -18,8 +18,7 @@ create or alter table vacation_spots (
   , zoo_cnt int
   , korean_restaurant_cnt int
 -- ) data_retention_time_in_days = 1;
-  data_retention_time_in_days = {{retention_time}};
-
+) data_retention_time_in_days = {{retention_time}};
 
 -- task to merge pipeline results into target table
 create or alter task vacation_spots_update
